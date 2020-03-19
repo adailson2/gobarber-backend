6431748bc9da08d9502a5cf260436e63b161dde8
Adailson Santos <adailson2@gmail.com>
2020-03-19 01:40:34 -0300

    creating nodejs struture and configure sucrase and nodemon

---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+import e, { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Rocketseat'});
+})
+
+export default routes; 
