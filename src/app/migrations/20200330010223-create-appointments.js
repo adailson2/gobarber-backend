f0f27f01abce7053c32f76e36abb8a895ccdab8c
Adailson Santos <adailson2@gmail.com>
2020-03-29 22:36:32 -0300

    services appointments

--- src/app/migrations/20200330010223-create-appointments.js
+++ src/app/migrations/20200330010223-create-appointments.js
@@ -20,6 +20,13 @@ module.exports = {
         onDelete: 'SET NULL',
         allowNull: true,
       },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
       canceled_at: {
         type: Sequelize.DATE,
       },
