7270efdb2d730986caaf1681bb1793d8f5a85e1b
Adailson Santos <adailson2@gmail.com>
2020-03-31 14:48:32 -0300

    listing available schedules

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -10,7 +10,7 @@ class CancelationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    await Mail.senddMail({
+    await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
       template: 'cancellation',
