08699fe1e7e0ee21b3e5afe085097cf7dd372a7b
Adailson Santos <adailson2@gmail.com>
2020-03-31 12:27:11 -0300

    config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e35be6a461ed3e',
+    pass: '088d59316395e5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
